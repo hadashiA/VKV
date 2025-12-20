2a6852d9289d57b5a1c0eb020845e18936118ff0
hadashiA <dev@hadashikick.jp>
2025-12-20 17:34:58 +0900

    Add public api

--- src/VKV.MessagePack/ReadOnlyTableExtensions.cs
+++ src/VKV.MessagePack/ReadOnlyTableExtensions.cs
@@ -4,6 +4,14 @@ namespace VKV.MessagePack;
 
 public static class ReadOnlyTableExtensions
 {
+    public static MessagePackReadOnlyTable<TValue> GetTable<TValue>(
+        this ReadOnlyDatabase db,
+        string tableName,
+        MessagePackSerializerOptions? options = null)
+    {
+        return db.GetTable(tableName).AsMessagePackSerializable<TValue>(options);
+    }
+
     public static MessagePackReadOnlyTable<TValue> AsMessagePackSerializable<TValue>(
         this ReadOnlyTable table,
         MessagePackSerializerOptions? options = null)
